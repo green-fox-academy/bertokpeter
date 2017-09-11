6c2f7b4f7b47eebbcb95f0a8ee5ecfe7e4c08acd
bertokpeter <bertokpeter92@gmail.com>
2017-09-11 13:08:19 +0200

    feat(define)Task10complete

---
+++ week-02/day-01/define_basic_info.py
@@ -0,0 +1,14 @@
+# Define several things as a variable than print their values
+# Your name as a string
+# Your age as an integer
+# Your height in meters as a double
+# Wether you are married or not as a boolean
+name = "Peter Bertram"
+age = 25
+height = 1.84
+married = False
+
+print(name)
+print(age)
+print(height)
+print(married)
