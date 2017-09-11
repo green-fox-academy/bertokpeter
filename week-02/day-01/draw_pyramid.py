9791c23409362a91a2c73b5b94ca8b991976d6cd
bertokpeter <bertokpeter92@gmail.com>
2017-09-11 16:16:56 +0200

    feat(pyramid)Task29complete

---
+++ week-02/day-01/draw_pyramid.py
@@ -0,0 +1,13 @@
+# Write a program that reads a number from the standard input, then draws a
+# pyramid like this:
+#
+#
+#    *
+#   ***
+#  *****
+# *******
+#
+# The pyramid should have as many lines as the number was
+a = int(input("Please enter a number: "))
+for i in range(1,a+1):
+    print(" "*(a-i) + "*"*(i*2-1) + " "*(a-i))
