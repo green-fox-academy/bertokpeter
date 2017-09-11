6b19cfcc0c164b109e18a2a8a7881d027a234c01
bertokpeter <bertokpeter92@gmail.com>
2017-09-11 14:43:06 +0200

    feat(print_bigger)Task20complete

---
+++ week-02/day-01/print_bigger.py
@@ -0,0 +1,7 @@
+# Write a program that asks for two numbers and prints the bigger one
+a = int(input("First number: "))
+b = int(input("Second number: "))
+if a > b:
+    print("The bigger is: " + str(a))
+else:    
+    print("The bigger is: " + str(b))
