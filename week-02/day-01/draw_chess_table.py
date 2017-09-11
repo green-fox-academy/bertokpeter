8b9fc7d122a8fbc223e97288bd2169a7f81fa468
bertokpeter <bertokpeter92@gmail.com>
2017-09-11 21:52:50 +0200

    feat(chess)Task35complete

---
+++ week-02/day-01/draw_chess_table.py
@@ -0,0 +1,26 @@
+# Crate a program that draws a chess table like this
+#
+# % % % % 
+#  % % % %
+# % % % %
+#  % % % %
+# % % % %
+#  % % % %
+# % % % % 
+#  % % % %
+#
+a = int(input("Please enter a number: "))
+if a == 1:
+    print("%")
+elif a%2 == 0:
+    for i in range(1,a+1):
+        if i%2 == 1:
+            print(("%"+" ")*(a//2))
+        else:
+            print((" "+"%")*(a//2))
+else:
+    for i in range(1,a+1):
+        if i%2 == 1:
+            print(("%"+" ")*(a//2)+"%")
+        else:
+            print((" "+"%")*(a//2)+" ")
