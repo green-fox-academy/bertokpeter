13b3ce47a04fcc2460a5f0de4e863ecf8fd4f4f1
bertokpeter <bertokpeter92@gmail.com>
2017-09-11 17:56:57 +0200

    feat(square)task31complete

---
+++ week-02/day-01/draw_square.py
@@ -0,0 +1,20 @@
+# Write a program that reads a number from the standard input, then draws a
+# square like this:
+#
+#
+# %%%%%
+# %   %
+# %   %
+# %   %
+# %   %
+# %%%%%
+#
+# The square should have as many lines as the number was
+a = int(input("Please enter a number: "))
+for i in range(1,(a+1)):
+    if i==1:
+        print("%"*(a-1))
+    elif i<a:
+        print("%"+" "*(a-3)+"%")
+    else:
+        print("%"*(a-1))
