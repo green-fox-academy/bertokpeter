c17f1f3090a846c565a37efc578399c6118c2c20
bertokpeter <bertokpeter92@gmail.com>
2017-09-11 14:39:16 +0200

    feat(one_two)Task19complete

---
+++ week-02/day-01/one_two_a_lot.py
@@ -0,0 +1,15 @@
+# Write a program that reads a number form the standard input,
+# If the number is zero or smaller it should print: Not enough
+# If the number is one it should print: One
+# If the number is two it should print: Two
+# If the number is more than two it should print: A lot
+a = int(input("Please write a number here: "))
+if a <= 0:
+    print("Not enough")
+elif a == 1:
+    print("One")
+elif a == 2:
+    print("Two")
+else:
+    print("A lot")
+
