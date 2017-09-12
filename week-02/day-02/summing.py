b2c8dd020d48dec511101f099838566404e2f356
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 11:22:53 +0200

    fix(sum)Simplified

--- week-02/day-02/summing.py
+++ week-02/day-02/summing.py
@@ -2,6 +2,5 @@
 #   until the given parameter
 input_number = int(input("Please input a number "))
 def sum_(number):
-    number_list= list(range(1,number+1))
-    return sum(number_list)
+    return sum(list(range(1,number+1)))
 print(sum_(input_number))
