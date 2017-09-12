4ff80093504f89df88c5aa34dfb7d4dc159d0f7e
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 12:07:28 +0200

    feat(change_elements)Task10complete

---
+++ week-02/day-02/change_element.py
@@ -0,0 +1,7 @@
+# - Create an array variable named `s`
+#   with the following content: `[1, 2, 3, 8, 5, 6]`
+# - Change the 8 to 4
+# - Print the fourth element
+s = list(range(1,7))
+s[3] = 4
+print(s[3])
