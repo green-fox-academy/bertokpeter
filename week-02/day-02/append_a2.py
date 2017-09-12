436c14eb31ea3f77e98e6835a228711db8a0dea5
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 14:38:57 +0200

    feat(append_a2)Task16complete

---
+++ week-02/day-02/append_a2.py
@@ -0,0 +1,7 @@
+# - Create a variable named `nimals`
+#   with the following content: `["kuty", "macs", "cic"]`
+# - Add all elements an `"a"` at the end
+nimals = ["kuty", "macsk", "cic"]
+for i in range(len(nimals)):
+    nimals[i]= nimals[i] + 'a'
+print(nimals)
