3c0070fc8b1bd84be3fefb104e948b3469739b29
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 10:30:58 +0200

    feat(doublung)Task01complete

---
+++ week-02/day-02/doubling.py
@@ -0,0 +1,7 @@
+# - Create a variable named `ak` and assign the value `123` to it
+# - Create a function called `doubling` that doubles it's input parameter
+# - Print the result of `doubling(ak)`
+ak = 123
+def doubling(number):
+    return number*2
+print(doubling(ak))
