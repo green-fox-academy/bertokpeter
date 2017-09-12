ffb5b9f466530e93a612a197f3e8d20090817b9c
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 15:00:50 +0200

    feat(reverse)Task19complete

---
+++ week-02/day-02/reverse_list.py
@@ -0,0 +1,6 @@
+# - Create a variable named `aj`
+#   with the following content: `[3, 4, 5, 6, 7]`
+# - Reverse the order of the elements in `aj`
+# - Print the elements of the reversed `aj`
+aj = list(range(3,8))
+print(list(reversed(aj)))    
