2ebdec511dd2d0685eedf6510b9922557d00ba8e
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 12:44:54 +0200

    feat(matrix)Task13complete

---
+++ week-02/day-02/matrix.py
@@ -0,0 +1,14 @@
+# - Create (dynamically) a two dimensional list
+#   with the following matrix. Use a loop!
+#
+#   1 0 0 0
+#   0 1 0 0
+#   0 0 1 0
+#   0 0 0 1
+#
+# - Print this two dimensional list to the output
+matrix = [[],[],[],[]]
+for i in range(0,4):
+    matrix[i] = [0,0,0,0]
+    matrix[i][i] += 1
+    print(matrix[i])
