119b016fd80cc61597453ceb8dd4ac784352640a
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 13:23:51 +0200

    fix(pyramid)deletewhitespaces

--- week-02/day-01/draw_pyramid.py
+++ week-02/day-01/draw_pyramid.py
@@ -10,4 +10,4 @@
 # The pyramid should have as many lines as the number was
 a = int(input("Please enter a number: "))
 for i in range(1,a+1):
-    print(" "*(a-i) + "*"*(i*2-1) + " "*(a-i))
+    print(" "*(a-i) + "*"*(i*2-1))
