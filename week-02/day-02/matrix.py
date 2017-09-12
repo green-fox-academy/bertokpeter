936b48239d50a02ccb96a732810c4382b43b1d2e
bertokpeter <bertokpeter92@gmail.com>
2017-09-12 12:48:31 +0200

    fix(matrix)fixedlooplength

--- week-02/day-02/matrix.py
+++ week-02/day-02/matrix.py
@@ -8,7 +8,7 @@
 #
 # - Print this two dimensional list to the output
 matrix = [[],[],[],[]]
-for i in range(0,4):
+for i in range(0,len(matrix)):
     matrix[i] = [0,0,0,0]
     matrix[i][i] += 1
     print(matrix[i])
