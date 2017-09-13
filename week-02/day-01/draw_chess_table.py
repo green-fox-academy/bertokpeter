7078b90f5f556b70c30baad56eeed90d63551895
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 11:22:10 +0200

    fix(chess)What to do if number is negative

--- week-02/day-01/draw_chess_table.py
+++ week-02/day-01/draw_chess_table.py
@@ -10,17 +10,20 @@
 #  % % % %
 #
 a = int(input("Please enter a number: "))
-if a == 1:
-    print("%")
-elif a%2 == 0:
-    for i in range(1,a+1):
-        if i%2 == 1:
-            print(("%"+" ")*(a//2))
-        else:
-            print((" "+"%")*(a//2))
-else:
-    for i in range(1,a+1):
-        if i%2 == 1:
-            print(("%"+" ")*(a//2)+"%")
-        else:
-            print((" "+"%")*(a//2)+" ")
+while a <= 0:
+    a = int(input("Please enter a bigger number: "))
+if a > 0:
+    if a == 1:
+        print("%")
+    elif a%2 == 0:
+        for i in range(1,a+1):
+            if i%2 == 1:
+                print(("%"+" ")*(a//2))
+            else:
+                print((" "+"%")*(a//2))
+    else:
+        for i in range(1,a+1):
+            if i%2 == 1:
+                print(("%"+" ")*(a//2)+"%")
+            else:
+                print((" "+"%")*(a//2)+" ")
