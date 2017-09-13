c1e3c7b3bc633b7ec72420d333e9e9b45b2e8008
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 11:16:32 +0200

    fix(parametric)Number of numbers is now input

--- week-02/day-01/parametric_average.py
+++ week-02/day-01/parametric_average.py
@@ -4,10 +4,14 @@
 # integers like:
 #
 # Sum: 22, Average: 4.4
-many = 6
-a = [0]*many
-for i in range(0,many):
-    a[i] = int(input("Enter a number ")) 
-Sum = sum(a)
-Average = Sum/many
-print("Sum: " + str(Sum) + ", Avergae:" + str(Average))
+sum_list = []
+many = int(input("How many number would you like? "))
+while many <= 0:
+    many = int(input("Your number should be higher "))
+if many > 0:    
+    for i in range(many):
+        number = int(input("Enter a number "))
+        sum_list.append(number)
+    Sum = sum(sum_list)
+    Average = Sum/many
+    print("Sum: " + str(Sum) + ", Avergae:" + str(Average))
