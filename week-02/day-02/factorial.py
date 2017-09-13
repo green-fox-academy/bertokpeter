3ee90f11a21e9ebc68fea54c44f65120e15500af
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 11:39:02 +0200

    fix(summing)Now writes 0 for negative numbers

--- week-02/day-02/factorial.py
+++ week-02/day-02/factorial.py
@@ -2,8 +2,11 @@
 #   that returns it's input's factorial
 input_number = int(input("Please input a number "))
 def factorio(number):
-    factorial = 1
-    for i in range(1, number+1):
-        factorial = i*factorial
-    return factorial
+    if number < 0:
+        return 0
+    else:
+        factorial = 1
+        for i in range(1, number+1):
+            factorial = i*factorial
+        return factorial
 print(factorio(input_number))
