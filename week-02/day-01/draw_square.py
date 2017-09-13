f84ed9e746d678476062da4dc4286a4de680f684
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 10:59:43 +0200

    fix(square)Deleted duplication

--- week-02/day-01/draw_square.py
+++ week-02/day-01/draw_square.py
@@ -12,9 +12,7 @@
 # The square should have as many lines as the number was
 a = int(input("Please enter a number: "))
 for i in range(1,(a+1)):
-    if i==1:
+    if i==1 or i == a:
         print("%"*(a-1))
-    elif i<a:
-        print("%"+" "*(a-3)+"%")
     else:
-        print("%"*(a-1))
+        print("%"+" "*(a-3)+"%")
