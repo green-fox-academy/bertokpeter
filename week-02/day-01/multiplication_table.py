2cee54e926c3ec76c78c9a4f064267b7afa805fb
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 10:40:27 +0200

    fix(multiplication)Deleted a whitespace

--- week-02/day-01/multiplication_table.py
+++ week-02/day-01/multiplication_table.py
@@ -15,4 +15,4 @@
 # 10 * 15 = 150
 a = int(input("Enter a number here: "))
 for i in range(1, 11):
-    print(str(i) + " * " + str(a) + " = " + str(i*a) )
+    print(str(i) + " * " + str(a) + " = " + str(i*a))
