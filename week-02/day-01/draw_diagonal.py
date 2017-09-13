7f79f3d261f28d40f10db3eb9789dee02c8aa861
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 11:06:11 +0200

    fix(diagonal)Deleted duplication

--- week-02/day-01/draw_diagonal.py
+++ week-02/day-01/draw_diagonal.py
@@ -12,11 +12,9 @@
 # The square should have as many lines as the number was
 a = int(input("Please enter a number: "))
 for i in range(1,a+1):
-    if i == 1:
+    if i == 1 or i == a:
         print("%"*(a-1))
     elif i < a-1:
         print("%"+" "*(i-2)+"%"+" "*(a-2-i)+"%")
-    elif i < a:
-        print("%"+" "*(a-3)+"%")
     else:
-        print("%"*(a-1))
+        print("%"+" "*(a-3)+"%")
