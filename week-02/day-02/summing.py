88eb3819c90b07a92a6535557ec264ee87300c2f
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 11:36:05 +0200

    fix(summing)Now can sum negative numbers

--- week-02/day-02/summing.py
+++ week-02/day-02/summing.py
@@ -2,5 +2,8 @@
 #   until the given parameter
 input_number = int(input("Please input a number "))
 def sum_(number):
-    return sum(list(range(1,number+1)))
+    if number > 0:
+        return sum(list(range(1,number+1)))
+    else:
+        return sum(list(range(number,0)))
 print(sum_(input_number))
