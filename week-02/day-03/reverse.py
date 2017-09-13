0144fad07488d956096e88329f6da37ee20f420a
bertokpeter <bertokpeter92@gmail.com>
2017-09-13 12:16:51 +0200

    feat(reverese)Task02 complete

---
+++ week-02/day-03/reverse.py
@@ -0,0 +1,7 @@
+# Reverse the string using StringBuilder
+
+reverse = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI"
+# Solution 1
+print(reverse[::-1])
+# Solution 2
+print("".join(reversed(reverse)))
