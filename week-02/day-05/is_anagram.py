42151cdb59d21e838941528fc6933435fb9dac6f
bertokpeter <bertokpeter92@gmail.com>
2017-09-15 10:26:47 +0200

    feat(anagram)Added anagram checker

--- week-02/day-05/is_anagram.py
+++ week-02/day-05/is_anagram.py
@@ -1,16 +1,19 @@
 anagram1 = input("Please enter your first word here: ")
 anagram2 = input("Please enter your second word here: ")
 def string_to_list(text):
-    length = len(text)
     word = []
-    for i in range(length):
+    for i in range(len(text)):
         word.append(text[i])
     return word
 def is_anagram(tex1, text2):
     word1 = string_to_list(tex1)
     word2 = string_to_list(text2)
-    if length1 == length2:
-        sum = []
-        for i in range(length1):
-            if text1[i] in text2:
-                sum.append(
+    if len(word1) == len(word2):
+        for i in word1:
+            if i in word2:
+                word2.remove(i)
+    if len(word2) == 0:
+        print("The words are anagrams")
+    else:
+        print("These words aren't anagrams")
+is_anagram(anagram1,anagram2)
