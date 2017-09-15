2b974d2ac5e7c3309d4e86ceb1d75a59acef72f8
bertokpeter <bertokpeter92@gmail.com>
2017-09-15 12:23:36 +0200

    feat(guess)Number guesser added

---
+++ week-02/day-05/guess_number.py
@@ -0,0 +1,21 @@
+import random
+random_range = int(input("Enter the biggest number you would like to guess: "))
+def guess(range):
+    number = random.randint(1,range)
+    lives = int(input("How many lives do you want? "))
+    print("I've the number between 1-" + str(range) + ". You have " + str(lives) + " lives.")
+    a = int(input("Can you guess the number? "))
+    while a != number and lives > 0:
+        print("Guess again... ")
+        lives -= 1
+        if lives == 0:
+            print("Sorry you died. Better luck next time!")
+        elif a < number:
+            print("Too low. You've " + str(lives) + "left.")
+            a = int(input("Can you guess the number? "))
+        else:
+            print("Too high. You've " + str(lives) + "left.")
+            a = int(input("Can you guess the number? "))
+    if a == number:
+        print("Congratulations! You found the number: " + str(number))
+guess(random_range)
