e1e533be4d758c4203eff6a5e2ce0b8f2f9cf7ea
bertokpeter <bertokpeter92@gmail.com>
2017-09-17 16:26:38 +0200

    fix(guess)Some spaces added

--- week-02/day-05/guess_number.py
+++ week-02/day-05/guess_number.py
@@ -11,10 +11,10 @@ def guess(range):
         if lives == 0:
             print("Sorry you died. Better luck next time!")
         elif a < number:
-            print("Too low. You've " + str(lives) + "lives left.")
+            print("Too low. You've " + str(lives) + " lives left.")
             a = int(input("Can you guess the number? "))
         else:
-            print("Too high. You've " + str(lives) + "lives left.")
+            print("Too high. You've " + str(lives) + " lives left.")
             a = int(input("Can you guess the number? "))
     if a == number:
         print("Congratulations! You found the number: " + str(number))
