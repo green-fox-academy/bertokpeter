8d10b3140f7a5060500ca86b9203de264236df31
bertokpeter <bertokpeter92@gmail.com>
2017-09-17 16:52:55 +0200

    feat(palindrome_searcher)added new reverse function

--- week-02/day-05/palindrome_searcher.py
+++ week-02/day-05/palindrome_searcher.py
@@ -1,24 +1,13 @@
 input_text = input("Please enter your text here: ")
-def string_to_list(text):
-    word = []
-    for i in range(len(text)):
-        word.append(text[i])
-    return word
-def create_palindrome(string):
-    word_list = string_to_list(string)
-    length = len(word_list)
-    palindrome = ""
-    while length > 0:
-        palindrome = palindrome + word_list[length-1]
-        length -= 1
-    return palindrome
+def reverse_string(text):
+    return text[::-1]
 def palindrome_searcher(long_text):
     lowercase = long_text.lower()
-    palindrome = create_palindrome(lowercase)
+    palindrome = reverse_string(lowercase)
     length = len(lowercase)
     palindrome_list = []
     for i in range(length-2):
-        for j in range(3,length + 1):
+        for j in range(3,length+1):
             if lowercase[i:i+j] == palindrome[length-(i+j):length-i]:
                 palindrome_list.append(lowercase[i:i+j])
     return palindrome_list
