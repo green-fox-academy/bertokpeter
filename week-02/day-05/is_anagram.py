cae3a8094aa1ba36507df7f30b0541f4b08b7469
bertokpeter <bertokpeter92@gmail.com>
2017-09-17 16:30:06 +0200

    fix(angram)indentation fixed back

--- week-02/day-05/is_anagram.py
+++ week-02/day-05/is_anagram.py
@@ -12,10 +12,8 @@ def is_anagram(tex1, text2):
         for i in word1:
             if i in word2:
                 word2.remove(i)
-        if len(word2) == 0:
-            print("The words are anagrams")
-        else:
-            print("These words aren't anagrams")
+    if len(word2) == 0:
+        print("The words are anagrams")
     else:
         print("These words aren't anagrams")
 is_anagram(anagram1,anagram2)
