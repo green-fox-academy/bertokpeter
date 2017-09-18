294c0731cba230394d15717362ac90728c43048f
bertokpeter <bertokpeter92@gmail.com>
2017-09-18 13:33:43 +0200

    fix(print)modified filepath

--- week-03/day-01/print_each_line.py
+++ week-03/day-01/print_each_line.py
@@ -2,7 +2,7 @@
 # each of lines form the file.
 # If the program is unable to read the file (for example it does not exists),
 # then it should print an error message like: "Unable to read file: my-file.txt"
-my_file = "my-file.txt"
+my_file = "week-03/day-01/my-file.txt"
 def line_printer(file_name):
     try:
         file = open(file_name, "r")
