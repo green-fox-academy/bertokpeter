0588ad6def31f192b311a93fbf39337d527fc249
bertokpeter <bertokpeter92@gmail.com>
2017-09-18 11:54:19 +0200

    feat(line_counter)Line counter complete

---
+++ week-03/day-01/count_lines.py
@@ -0,0 +1,15 @@
+# Write a function that takes a filename as string,
+# then returns the number of lines the file contains.
+# It should return zero if it can't open the file, and
+# should not raise any error.
+my_file = "my-file.txt"
+def line_counter(file_name):
+    try:
+        file = open(file_name, "r")
+        print(len(file.readlines()))
+        file.close()
+    except IOError:
+        print("Unable to read file: " + file_name)
+    except:
+        print("Error")
+line_counter(my_file)
