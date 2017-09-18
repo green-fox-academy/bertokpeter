200491bf4d30587724bd52d29370643ff85ba738
bertokpeter <bertokpeter92@gmail.com>
2017-09-18 17:37:08 +0200

    feat(rev_order)Function reverses order

---
+++ week-03/day-01/reversed_order.py
@@ -0,0 +1,10 @@
+# Create a method that decrypts reversed-order.txt
+rev_ordered_file = "week-03/day-01/reversed-order.txt"
+def reverse_order(file_name):
+    line_list = []
+    with open(file_name, "r") as f1:
+        for line in f1:
+            line_list.append(line)
+    for i in line_list[::-1]:
+        print(i)
+reverse_order(rev_ordered_file)
