964af1b78e100ef0a2421a7e29c75dec5000f1b8
bertokpeter <bertokpeter92@gmail.com>
2017-09-18 17:00:14 +0200

    feat(reversed_lines)Added function that reverses text from file

---
+++ week-03/day-01/reversed_lines.py
@@ -0,0 +1,7 @@
+# Create a method that decrypts reversed-lines.txt
+reversed_file = "week-03/day-01/reversed_lines.txt"
+def reverse(file_name):
+    with open(file_name, "r") as f1:
+        for line in f1:
+            print(line[::-1])
+reverse(reversed_file)
