2eaec67008048c2aacd129b06f6ad69b25344cb7
bertokpeter <bertokpeter92@gmail.com>
2017-09-19 18:32:22 +0200

    fix(doubled)Removed unneccesary 'else'

--- week-03/day-01/doubled.py
+++ week-03/day-01/doubled.py
@@ -3,11 +3,10 @@ crypted_file = "week-03/day-01/duplicated-chars.txt"
 def decrypt(file_name):
     new_file = ""
     with open(file_name, "r") as f1:
-        whole_file = f1.read()
-    for i in range(len(whole_file)-1):
-        if whole_file[i] == whole_file[i+1]:
-            pass
-        else:
-            new_file = new_file + whole_file[i]
-    print(new_file)
+        for line in f1:
+            for i in range(len(line)-1):
+                if line[i] == line[i+1]:
+                    new_file = new_file + line[i]    
+            print(new_file)
+            new_file = ""
 decrypt(crypted_file)
