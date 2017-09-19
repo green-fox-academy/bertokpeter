c17cb281769eda35c2e5dd0a9d2886868cb4478c
bertokpeter <bertokpeter92@gmail.com>
2017-09-19 18:34:43 +0200

    fix(rev_lines):Now looks over newline chars

--- week-03/day-01/reversed_lines.py
+++ week-03/day-01/reversed_lines.py
@@ -3,5 +3,5 @@ reversed_file = "week-03/day-01/reversed_lines.txt"
 def reverse(file_name):
     with open(file_name, "r") as f1:
         for line in f1:
-            print(line[::-1])
+            print(line[::-1], end='')
 reverse(reversed_file)
