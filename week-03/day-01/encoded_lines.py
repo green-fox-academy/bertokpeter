87225843c0b021a0f53c093332dc169a977162ea
bertokpeter <bertokpeter92@gmail.com>
2017-09-19 19:25:42 +0200

    feat(encoded):decrypt function complete

--- week-03/day-01/encoded_lines.py
+++ week-03/day-01/encoded_lines.py
@@ -1,22 +1,20 @@
 # Create a method that decrypts encoded-lines.txt
-import string
 encoded_file = "week-03/day-01/encoded_lines.txt"
 def decrypt(file_name):
     decoded_line = ""
     with open(file_name, "r") as f1:
         for line in f1:
             for char in line:
-
+                decoded_line += swap(char)
+            print(decoded_line, end='')
+            decoded_line = ""
 
 def swap(char):
-    decoder = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[,-./'(*+"
-    actual_index = decoder.index(char)
-    return 	string.ascii_lowercase[actual_index-1]
-
+    decoder = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ[,-./'(*+!\""
+    if char in decoder:
+        actual_index = decoder.index(char)
+        return 	decoder[actual_index-1]
+    else:
+        return char
 
 decrypt(encoded_file)
-str = "bmnb"
-out = ""
-for char in str:
-	out += swap(char)
-print(out)
