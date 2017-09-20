7b4710a4c2318c7e6f3e81394d9c905239c9e899
bertokpeter <bertokpeter92@gmail.com>
2017-09-20 11:55:20 +0200

    fix(diagonals)Lines are now green

--- week-03/day-03/diagonals.py
+++ week-03/day-03/diagonals.py
@@ -6,10 +6,10 @@ canvas_width = 300
 canvas_height = 300
 canvas = Canvas(root, width=canvas_width, height=canvas_height)
 canvas.pack()
-canvas_height_middle = canvas_height/2
+middle = canvas_height/2
 
 # draw the canvas' diagonals in green.
 for i in (1, -1):
-    canvas.create_line(0, canvas_height_middle - canvas_height_middle*i, canvas_width, canvas_height_middle - canvas_height_middle*-i)
+    canvas.create_line(0, middle - middle*i, canvas_width, middle - middle*-i, fill="green")
 
 root.mainloop()
