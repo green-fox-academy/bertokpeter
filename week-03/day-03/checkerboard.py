6facf940a5ba8d49cf0e120ac82bd1f4e6f353e0
bertokpeter <bertokpeter92@gmail.com>
2017-09-20 15:59:31 +0200

    feat(checkerboard):draws checkboard

---
+++ week-03/day-03/checkerboard.py
@@ -0,0 +1,32 @@
+from tkinter import *
+
+root = Tk()
+
+canvas_width = 300
+canvas_height = 300
+canvas = Canvas(root, width=canvas_width, height=canvas_height)
+canvas.pack()
+
+# fill the canvas with a checkerboard pattern.
+
+def draw_checkboard(row_size):
+    number_of_rows = row_size
+    size = canvas_width/row_size
+    start_width = 0
+    start_height = 0
+    for i in range(number_of_rows):
+        for j in range(number_of_rows+1//2):
+            end_width = start_width + size
+            end_height = start_height + size
+            coords = [start_width, start_height, end_width, end_height]
+            canvas.create_rectangle(coords, fill="black")
+            start_width = end_width + size
+        if i%2 == 0:
+            start_width = size
+        if i%2 == 1:
+            start_width = 0
+        start_height += size 
+
+draw_checkboard(8)
+
+root.mainloop()
