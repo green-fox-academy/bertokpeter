085500cf7eaaa72e84b8ab6048461c9b02c27773
bertokpeter <bertokpeter92@gmail.com>
2017-09-20 11:29:19 +0200

    feat(colored_box): added colored lines

--- week-03/day-03/colored_box.py
+++ week-03/day-03/colored_box.py
@@ -7,6 +7,10 @@ canvas.pack()
 
 # draw a box that has different colored lines on each edge.
 box_coord = [50, 50, 250, 250]
-box = canvas.create_rectangle(box_coord, fill="red")
+box = canvas.create_rectangle(box_coord, fill="grey")
+canvas.create_line(box_coord[0], box_coord[0], box_coord[2], box_coord[0], fill="purple", width=3)
+canvas.create_line(box_coord[0], box_coord[0], box_coord[0], box_coord[2], fill="red", width=3)
+canvas.create_line(box_coord[0], box_coord[2], box_coord[2], box_coord[2], fill="blue", width=3)
+canvas.create_line(box_coord[2], box_coord[0], box_coord[2], box_coord[2], fill="green", width=3)
 
 root.mainloop()
