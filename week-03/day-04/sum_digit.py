6c6e8277d574487fb34a8f54a8058f796bb6bd46
bertokpeter <bertokpeter92@gmail.com>
2017-09-21 11:25:47 +0200

    feat(sum_digit):Tries and catches recursionerror

--- week-03/day-04/sum_digit.py
+++ week-03/day-04/sum_digit.py
@@ -2,14 +2,20 @@
 # Note that mod (%) by 10 yields the rightmost digit (126 % 10 is 6), while 
 # divide (/) by 10 removes the rightmost digit (126 // 10 is 12).
 
-def sum_digits(number):
-    log10 = len(str(number))-1
-    if len(str(number)) == 1:
-        return number
-    else:
-        return number//(10**log10) + sum_digits(number%(10**log10))
+try:
+    def sum_digits(number):
+        if number < 0:
+            return "number must be positive"
+        else:    
+            log10 = len(str(number))-1
+            if len(str(number)) == 1:
+                return number
+            else:
+                return number//(10**log10) + sum_digits(number%(10**log10))
 
-print(sum_digits(985626))
+    print(sum_digits(9856.26))
+except RecursionError:
+    print("number must be integer not float")
 
 def sum_digits_ati(number):
     if number == 0:
