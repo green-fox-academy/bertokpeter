be7773a41c35faac3633ff420d36e16dbe5d282d
bertokpeter <bertokpeter92@gmail.com>
2017-09-21 12:23:21 +0200

    feat(string1):Function replaces x to y with recursion

--- week-03/day-04/string1.py
+++ week-03/day-04/string1.py
@@ -1,12 +1,18 @@
 # Given a string, compute recursively (no loops) a new string where all the
 # lowercase 'x' chars have been changed to 'y' chars.
+try:
+    def x_to_y(text):
+        new_text = ""
+        if len(text) == 0:
+            return new_text
+        else:
+            if text[0] == "x":
+                new_text = new_text + "y"
+                return new_text + x_to_y(text[1:])
+            else:
+                new_text = new_text + text[0]
+                return new_text + x_to_y(text[1:])
 
-def x_to_y(text):
-    new_text = ""
-    if not "x" in text:
-        return new_text
-    else:
-        new_text = text.replace("x","y")
-        return new_text
-
-print(x_to_y("My ex exited the xmen x times"))
+    print(x_to_y("My ex exited the xmen x times and x is so ixxy I feel like x"))
+except TypeError:
+    print("text must be string")
