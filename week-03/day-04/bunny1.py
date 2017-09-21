0c9f17e620687005faa817001f5af28806cf0cf4
bertokpeter <bertokpeter92@gmail.com>
2017-09-21 11:53:38 +0200

    fix(bunny1):Don't slice up the bunnies!

--- week-03/day-04/bunny1.py
+++ week-03/day-04/bunny1.py
@@ -2,9 +2,13 @@
 # We want to compute the total number of ears across all the bunnies recursively (without loops or multiplication).
 
 def bunny_ears(num_of_bunnys):
-    if num_of_bunnys == 1:
+    if not num_of_bunnys%1 == 0:
+        return "Don't slice up the bunnies!"
+    elif num_of_bunnys < 0:
+        return 0
+    elif num_of_bunnys == 1:
         return 2
     else:
         return 2 + bunny_ears(num_of_bunnys-1)
 
-print(bunny_ears(36.5))
+print(bunny_ears(35.5))
