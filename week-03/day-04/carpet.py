95a1cc989ae24b261223fceb207e09d5a2c64698
bertokpeter <bertokpeter92@gmail.com>
2017-09-21 14:57:01 +0200

    fix(carpet)Short function names

--- week-03/day-04/carpet.py
+++ week-03/day-04/carpet.py
@@ -8,7 +8,7 @@ canvas = Canvas(root, width=canvas_width, height=canvas_height, bg="yellow")
 canvas.pack()
 width = canvas_width/3
 
-def fractal_carpet(x,y,width):
+def carpet(x,y,width):
     if width < 2:
         pass
     else:
@@ -18,7 +18,7 @@ def fractal_carpet(x,y,width):
         canvas.create_line(start_x+width,y,start_x+width,y+width*3)
         canvas.create_line(x,start_y,x+width*3,start_y)
         canvas.create_line(x,start_y+width,x+width*3,start_y+width)
-        return fractal_carpet(start_x,y,width/3), fractal_carpet(x,start_y,width/3), fractal_carpet(start_x+width,start_y,width/3), fractal_carpet(start_x,start_y+width,width/3)
-fractal_carpet(0,0,width)
+        return carpet(start_x,y,width/3), carpet(x,start_y,width/3), carpet(start_x+width,start_y,width/3), carpet(start_x,start_y+width,width/3)
+carpet(0,0,width)
 
 root.mainloop()
