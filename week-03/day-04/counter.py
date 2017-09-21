81e5b1ab3bd57f51c1a8fb6644316f1a6be0358a
bertokpeter <bertokpeter92@gmail.com>
2017-09-21 10:54:01 +0200

    feat(counter):Tries and catches recursion error

--- week-03/day-04/counter.py
+++ week-03/day-04/counter.py
@@ -1,9 +1,11 @@
 # Write a recursive function that takes one parameter: n and counts down from n.
+try:
+    def counter(number):
+        if number == 0:
+            return 0
+        else:
+            return str(number) + " " + str(counter(number-1))
 
-def counter(number):
-    if number == 0:
-        return 0
-    else:
-        return str(number) + " " + str(counter(number-1))
-
-print(counter(100))
+    print(counter(100))
+except RecursionError:
+    print("number must be integer and positive")
