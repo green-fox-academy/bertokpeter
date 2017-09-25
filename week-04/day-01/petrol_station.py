f8acdab6a6942277d841faf3fae9935e6638d1dd
bertokpeter <bertokpeter92@gmail.com>
2017-09-25 15:13:24 +0200

    feat(petrol): Classes and functions added

---
+++ week-04/day-01/petrol_station.py
@@ -0,0 +1,13 @@
+class Station(object):
+    def __init__(self):
+        self.gas_amount = 500.0
+    
+    def refill(self, car):
+        self.gas_amount -= car.capacity
+        car.gas_amount += car.capacity
+        return self.gas_amount
+
+class Car(object):
+    def __init__(self):
+        self.gas_amount = 0
+        self.capacity = 100
