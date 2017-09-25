e039cf8b2a4dc021f331da8114f0ca9c1a942062
bertokpeter <bertokpeter92@gmail.com>
2017-09-25 10:36:07 +0200

    feat(post-it):Post-it class with three objects

---
+++ week-04/day-01/post-it.py
@@ -0,0 +1,27 @@
+# Create a PostIt class that has
+#   a background_color
+#   a text on it
+#   a text_color
+# Create a few example post-it objects:
+#   an orange with blue text: "Idea 1"
+#   a pink with black text: "Awesome"
+#   a yellow with green text: "Superb!"
+class PostIt:
+    background_color = ""
+    text = ""
+    text_color = ""
+
+idea = PostIt()
+idea.background_color = "orange"
+idea.text = "Idea 1"
+idea.text_color = "blue"
+
+awesome = PostIt()
+awesome.background_color = "pink"
+awesome.text = "Awesome"
+awesome.text_color = "black"
+
+superb = PostIt()
+superb.background_color = "yellow"
+superb.text = "Superb!"
+superb.text_color = "green"
