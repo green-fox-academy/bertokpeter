bd46652fbbda3810546e3312302ece8e9e794944
bertokpeter <bertokpeter92@gmail.com>
2017-09-25 14:40:52 +0200

    feat(counter):get and reset function

--- week-04/day-01/counter.py
+++ week-04/day-01/counter.py
@@ -1,7 +1,15 @@
 class Counter(object):
-    def __init__(self, field_value = 0):
-        self.field_value = field_value
+    def __init__(self, value = 0):
+        self.value = value
+        self.field_value = self.value
 
     def add(self, number = 1):
         self.field_value += number
+        return self.field_value
+
+    def get(self):
+        return self.field_value
+
+    def reset(self):
+        self.field_value = self.value
         return self.field_value
