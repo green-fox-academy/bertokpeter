ae086328312cc6d961e2cfa6cd7135be8f1ddff7
bertokpeter <bertokpeter92@gmail.com>
2017-09-25 14:33:29 +0200

    feat(counter):add function

--- week-04/day-01/counter.py
+++ week-04/day-01/counter.py
@@ -1,3 +1,7 @@
 class Counter(object):
     def __init__(self, field_value = 0):
         self.field_value = field_value
+
+    def add(self, number = 1):
+        self.field_value += number
+        return self.field_value
