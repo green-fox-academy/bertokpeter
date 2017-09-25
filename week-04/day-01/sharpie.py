5c2e516fc3756d8d3ceecc3e69deab8a196c8c41
bertokpeter <bertokpeter92@gmail.com>
2017-09-25 14:26:32 +0200

    feat(sharpie):Function decreases ink_amount

--- week-04/day-01/sharpie.py
+++ week-04/day-01/sharpie.py
@@ -2,8 +2,13 @@ class Sharpie(object):
     def __init__(self, color, width):
         self.color = str(color)
         self.width = float(width)
-        ink_amount = 100.0
+        self.ink_amount = 100.0
+
+    def use(self, amount):
+        self.ink_amount -= amount
+        return self.ink_amount
 
 sharp = Sharpie("medve",50)
 print(sharp.color)
-print(sharp.width)
+sharp.use(6.5)
+print(sharp.ink_amount)
