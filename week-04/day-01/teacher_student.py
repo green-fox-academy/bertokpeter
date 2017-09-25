d078f1bb08de00f9e2ea3700037abd41e5277565
bertokpeter <bertokpeter92@gmail.com>
2017-09-25 14:55:22 +0200

    feat(teacher):return values added

--- week-04/day-01/teacher_student.py
+++ week-04/day-01/teacher_student.py
@@ -1,14 +1,17 @@
 class Student(object):
     def learn(self):
         return "1+1=2"
-    
+
     def question(self, teacher):
-        teacher.answer()
+        return teacher.answer()
 
 class Teacher(object):
     def teach(self, student):
-        student.learn()
+        return student.learn()
 
     def answer(self):
         return "Hulye gyerek!"
 
+Peti = Student()
+Gyuri = Teacher()
+print(Peti.question(Gyuri))
