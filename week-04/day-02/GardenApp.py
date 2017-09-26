455b99db2f7819b49aeaeba4e45899d9278befa7
bertokpeter <bertokpeter92@gmail.com>
2017-09-26 11:32:11 +0200

    feat(Garden):water function

--- week-04/day-02/GardenApp.py
+++ week-04/day-02/GardenApp.py
@@ -2,3 +2,14 @@ class Garden(object):
     def __init__(self):
         self.flowers = []
         self.trees = []
+    
+    def water(self, amount):
+        self.to_water = []
+        for flower in self.flowers:
+            if flower.status == "needs water":
+                self.to_water.append(flower)
+        for tree in self.trees:
+            if tree.status == "needs water":
+                self.to_water.append(tree)
+        for plant in self.to_water:
+            amount/len(self.to_water)
