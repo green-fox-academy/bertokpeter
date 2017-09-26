7e8eadb1459f71dc505ffe4fefad71fe7bcb3469
bertokpeter <bertokpeter92@gmail.com>
2017-09-26 12:43:02 +0200

    feat(Garden):prints plants status and waters

--- week-04/day-02/GardenApp.py
+++ week-04/day-02/GardenApp.py
@@ -1,32 +1,24 @@
 class Garden(object):
     def __init__(self):
-        self.flowers = []
-        self.trees = []
-    
-    def add_flower(self, flower):
-        self.flowers.append(flower)
+        self.plants = []
 
-    def add_tree(self, tree):
-        self.trees.append(tree)
+    def add(self, plant):
+        self.plants.append(plant)
 
     def water(self, amount):
         print("Watering with {}".format(amount))
         self.to_water = []
-        for flower in self.flowers:
-            if flower.status() == "needs water":
-                self.to_water.append(flower)
-        for tree in self.trees:
-            if tree.status() == "needs water":
-                self.to_water.append(tree)
+        for plant in self.plants:
+            if plant.needs_water() == "needs water":
+                self.to_water.append(plant)
         for plant in self.to_water:
             plant.watered(amount/len(self.to_water))
+        print(self)
 
     def __str__(self):
         result = ""
-        for i in range(len(self.flowers)):
-             result += self.flowers[i].__str__() + "\n"
-        for i in range(len(self.trees)):
-             result += self.trees[i].__str__() + "\n"
+        for i in range(len(self.plants)):
+             result += self.plants[i].__str__() + "\n"
         return result
 
 class Tree(object):
@@ -38,7 +30,7 @@ class Tree(object):
         return "needs water" if self.water_amount < 10 else "doesn't need water"
 
     def __str__(self):
-        return "The {} Tree {}".format(self.color, self.needs_water)
+        return "The {} Tree {}".format(self.color, self.needs_water())
 
     def watered(self, amount):
         self.water_amount += amount*0.4    
@@ -48,13 +40,16 @@ class Flower(Tree):
         return "needs water" if self.water_amount < 5 else "doesn't need water"
 
     def __str__(self):
-        return "The {} Flower {}".format(self.color, self.needs_water)
+        return "The {} Flower {}".format(self.color, self.needs_water())
 
     def watered(self, amount):
         self.water_amount += amount*7.5    
 
 mygarden = Garden()
-mygarden.add_flower(Flower("yellow"))
-mygarden.add_flower(Flower("blue"))
-mygarden.add_tree(Tree("purple"))
-mygarden.add_tree(Tree("orange"))
+mygarden.add(Flower("yellow"))
+mygarden.add(Flower("blue"))
+mygarden.add(Tree("purple"))
+mygarden.add(Tree("orange"))
+print(mygarden)
+mygarden.water(40)
+mygarden.water(70)
