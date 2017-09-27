a9852c992f4dc05d52499e386d93210dff912cd2
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 14:15:59 +0200

    feat(test_anag):tests for different lengths

--- week-04/day-03/test_anagram.py
+++ week-04/day-03/test_anagram.py
@@ -4,9 +4,14 @@ from anagram import anagram
 class TestAnagram(unittest.TestCase):        
     
     def test_is_anagram(self):
-        word1 = "baba"
-        word2 = "baab"
+        word1 = "kocsi"
+        word2 = "ickso"
         self.assertTrue(anagram(word1, word2))
+    
+    def test_diff_length(self):
+        word1 = "bakocsi"
+        word2 = "kocsi"
+        self.assertFalse(anagram(word1, word2))
 
 if __name__=="__main__":
     unittest.main()
