bf085ef42b9bb7649d6195753139269ebe4c73c9
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 14:49:15 +0200

    feat(test_count): test for more letters

--- week-04/day-03/test_count_letters.py
+++ week-04/day-03/test_count_letters.py
@@ -6,6 +6,12 @@ class TestCountLetters(unittest.TestCase):
     def test_count_letters_one_letter(self):
         self.assertEqual(count_letters("a"),{"a":1})  
     
+    def test_count_letters_another_letter(self):
+        self.assertEqual(count_letters("b"),{"b":1})  
+    
+    def test_count_letters_more_letters(self):
+        self.assertEqual(count_letters("ab"),{"a":1,"b":1})  
+    
 
 
 if __name__=="__main__":
