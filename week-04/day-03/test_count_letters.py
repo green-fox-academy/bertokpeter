acf7af8d44efeff5d1e316d9e7b0d638a50c6557
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 15:18:46 +0200

    feat(test_count):tests for letters appearing multiple times

--- week-04/day-03/test_count_letters.py
+++ week-04/day-03/test_count_letters.py
@@ -12,7 +12,11 @@ class TestCountLetters(unittest.TestCase):
     def test_count_letters_more_letters(self):
         self.assertEqual(count_letters("ab"),{"a":1,"b":1})  
     
-
-
+    def test_count_letters_more_of_the_same_letters(self):
+        self.assertEqual(count_letters("aba"),{"a":2,"b":1})  
+    
+    def test_count_letters_for_long_word(self):
+        self.assertEqual(count_letters("babako"),{"b":2,"a":2,"k":1,"o":1})  
+    
 if __name__=="__main__":
     unittest.main()
