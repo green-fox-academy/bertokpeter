fd82cf20626ca69cec4b712f9de307dbc41f41b8
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 15:17:50 +0200

    feat(count):counts letters appearing multiple times

--- week-04/day-03/count_letters.py
+++ week-04/day-03/count_letters.py
@@ -1,5 +1,8 @@
 def count_letters(word):
     letters = {}
     for i in word:
-        letters[i] = 1
+        if i in letters:
+            letters[i] += 1
+        else:
+            letters[i] = 1
     return letters
