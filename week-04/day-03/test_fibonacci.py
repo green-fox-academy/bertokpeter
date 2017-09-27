5872918dd14ba500c29beb90b8282b618d3c6689
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 16:03:11 +0200

    feat(test_fibo):tests for typeerror

--- week-04/day-03/test_fibonacci.py
+++ week-04/day-03/test_fibonacci.py
@@ -15,6 +15,14 @@ class TestFibonacci(unittest.TestCase):
     def test_fibonacci_negative(self):
         self.assertEqual(fibonacci(-5), 0)
 
+    def test_fibonacci_string(self):
+        self.assertRaises(TypeError, fibonacci("apple"))  
+
+    def test_fibonacci_null(self):
+        self.assertRaises(TypeError, fibonacci(None))  
+
+    def test_fibonacci_float(self):
+        self.assertRaises(TypeError, fibonacci(5.5))  
     
 if __name__=="__main__":
     unittest.main()
