0e0289b55b8e0c2682443e144119f18bddf4495d
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 16:01:40 +0200

    feat(fibo):tries and catches TypeError

--- week-04/day-03/fibonacci.py
+++ week-04/day-03/fibonacci.py
@@ -1,7 +1,10 @@
 def fibonacci(number):
-    if number <= 0:
-        return 0
-    elif number == 1:
-        return 1
-    else:
-        return fibonacci(number-1) + fibonacci(number-2)
+    try:
+        if number <= 0:
+            return 0
+        elif number == 1:
+            return 1
+        else:
+            return fibonacci(number-1) + fibonacci(number-2)
+    except TypeError:
+        print("Number must be integer")
