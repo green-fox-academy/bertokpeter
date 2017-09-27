78514c01036a2b8f6ae303083b144f0a42a69703
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 16:48:40 +0200

    refact(extend):max and median function corrected

--- week-04/day-03/extend.py
+++ week-04/day-03/extend.py
@@ -4,14 +4,20 @@ def add(a, b):
 
 # Returns the highest value from the three given params
 def max_of_three(a, b, c):
-    if a > b:
+    if a > b and a > c:
         return a
+    elif b > a and b > c:
+        return b
     else:
         return c
 
 # Returns the median value of a list given as param
 def median(pool):
-    return pool[int((len(pool) - 1) / 2)]
+    pool.sort()
+    if len(pool)%2 == 0: 
+        return (pool[int(len(pool)/2-1)] + pool[int(len(pool)/2)])/2
+    else:
+        return pool[len(pool) // 2]
 
 # Returns true if the param is a vovel
 def is_vovel(char):
