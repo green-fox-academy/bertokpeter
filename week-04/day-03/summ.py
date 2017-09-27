24d61f6e085faf2398967d5b4d1a5273ac8dc5ba
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 13:42:36 +0200

    fix(summ):method can manage null values

--- week-04/day-03/summ.py
+++ week-04/day-03/summ.py
@@ -1,4 +1,7 @@
 class Sum(object):
 
     def summ(self, numbers):
+        for i in range(len(numbers)):
+            if numbers[i] == None:
+                numbers[i]=0
         return sum(numbers)
