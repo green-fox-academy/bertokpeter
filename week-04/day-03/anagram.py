c625dc50281b84db9474c112b9109d75341190a6
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 14:33:54 +0200

    feat(anag):now same length different letter words arent anagrams

--- week-04/day-03/anagram.py
+++ week-04/day-03/anagram.py
@@ -1,9 +1,11 @@
 def anagram(text1, text2):
     if len(text1) == len(text2):
-        new_text = ""
+        word_list = []
+        for i in text2:
+            word_list.append(i)
         for i in text1:
-            if i in text2:
-                new_text = new_text + i
-            if len(text2) == len(new_text):
+            if i in word_list:
+                word_list.remove(i)
+            if len(word_list) == 0:
                 return True
     return False
