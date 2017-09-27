0a65fa32080e00da26fe839be7736d08e3913987
bertokpeter <bertokpeter92@gmail.com>
2017-09-27 13:43:41 +0200

    fix(test_summ):method test null values

--- week-04/day-03/test_summ.py
+++ week-04/day-03/test_summ.py
@@ -12,6 +12,14 @@ class TestSum(unittest.TestCase):
     def test_empty_list(self):
         int_list = []
         self.assertEqual(summa.summ(int_list), 0)
+    
+    def test_one_element_list(self):
+        int_list = [5]
+        self.assertEqual(summa.summ(int_list), 5)
+    
+    def test_null_element_list(self):
+        int_list = [None, 5]
+        self.assertEqual(summa.summ(int_list), 5)
 
 if __name__=="__main__":
     unittest.main()
