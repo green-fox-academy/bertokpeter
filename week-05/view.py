cb66622cc608139b719e4c8191fc279f26d89596
bertokpeter <bertokpeter92@gmail.com>
2017-10-02 14:37:33 +0200

    feat(view):draw map draws wall tiles too

--- week-05/view.py
+++ week-05/view.py
@@ -6,13 +6,15 @@ class View:
         self.size = 720
         self.canvas = Canvas(self.root, width=self.size, height=self.size, bg="white", bd=0)
         self.floor_image = PhotoImage(file = "floor.png")
+        self.wall_image = PhotoImage(file = "wall.png")
         self.canvas.pack()
         self.canvas.focus_set()    
 
     def draw_map(self, map):
         for i in range(len(map)):
             for j in range(len(map[i])):
-                self.floor = self.canvas.create_image(j*72, i*72, anchor=NW, image=self.floor_image)
+                image = self.floor_image if map[i][j]==0 else self.wall_image
+                self.level = self.canvas.create_image(j*72, i*72, anchor=NW, image=image)
 
     def display(self):
         self.root.mainloop()
