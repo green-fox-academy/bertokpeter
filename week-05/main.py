83bae226d250dcff3ba033a36e045cec791e0a08
bertokpeter <bertokpeter92@gmail.com>
2017-10-04 11:11:12 +0200

    feat(main):draw skeletons

--- week-05/main.py
+++ week-05/main.py
@@ -6,7 +6,8 @@ class Game:
         self.mymap = Map()
         self.myview = View()
         self.myview.draw_map(self.mymap.tiles)
-        self.myview.draw_entity("down", 0, 0)
+        self.myview.draw_hero(self.myview.hero_down, 0, 0)
+        self.myview.draw_skeletons()
         self.myview.display()
 
 game = Game()
