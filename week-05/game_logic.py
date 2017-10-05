ef9e856cd91c25f62a5d345720ff8585ab960739
bertokpeter <bertokpeter92@gmail.com>
2017-10-05 15:43:28 +0200

    feat(logic):dead status options

--- week-05/game_logic.py
+++ week-05/game_logic.py
@@ -2,10 +2,14 @@ class Logic:
 
     def battle(self, attacker, defender):
         self.strike(attacker, defender)
-        self.strike(defender, attacker)
+        if not defender.status == "dead":
+            self.strike(defender, attacker)
 
     def strike(self, attacker, defender):
         sv = attacker.sp + attacker.dice()*2
         if sv > defender.dp:
             defender.currenthp -= (sv-defender.dp)
+            if defender.currenthp <= 0:
+                defender.currenthp = 0
+                defender.status = "dead"
         
