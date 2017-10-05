e52e317ea5c8f7cdc951aa0ca1eebebf7ab2bebb
bertokpeter <bertokpeter92@gmail.com>
2017-10-05 12:20:47 +0200

    feat(view):delete stats method

--- week-05/view.py
+++ week-05/view.py
@@ -36,7 +36,10 @@ class View:
         if char == "Hero":
             self.canvas.create_text(5,650, anchor=NW, font=24, text=text)
         else:
-            self.canvas.create_text(715,650, anchor=NE, font=24, text=text)
-        
+            self.canvas.create_text(715,650, anchor=NE, font=24, tag="monster_stats", text=text)
+    
+    def delete_stats(self):
+        self.canvas.delete("monster_stats")
+    
     def display(self):
         self.root.mainloop()
