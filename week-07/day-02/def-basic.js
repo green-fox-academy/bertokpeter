6e9090653e653d7dcdc134153637aa6d5faaa09e
bertokpeter <bertokpeter92@gmail.com>
2017-10-24 12:07:24 +0200

    fix(def-basic): semicolon added

--- week-07/day-02/def-basic.js
+++ week-07/day-02/def-basic.js
@@ -6,12 +6,12 @@
 // Your height in meters as a float
 // Whether you are married or not as a boolean
 
-let name = "Peter Bertram"
-let age = 25
-let height = 1.84
-let married = false
+let name = "Peter Bertram";
+let age = 25;
+let height = 1.84;
+let married = false;
 
-console.log(name)
-console.log(height)
-console.log(age)
-console.log(married)
+console.log(name);
+console.log(height);
+console.log(age);
+console.log(married);
