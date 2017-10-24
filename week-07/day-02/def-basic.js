60d3fe1f2ea9550956c0f775a298922d8c67009d
bertokpeter <bertokpeter92@gmail.com>
2017-10-24 12:03:36 +0200

    feat(def-basic): defines variables

---
+++ week-07/day-02/def-basic.js
@@ -0,0 +1,17 @@
+'use strict';
+
+// Define several things as a variable, then print their values
+// Your name as a string
+// Your age as an integer
+// Your height in meters as a float
+// Whether you are married or not as a boolean
+
+let name = "Peter Bertram"
+let age = 25
+let height = 1.84
+let married = false
+
+console.log(name)
+console.log(height)
+console.log(age)
+console.log(married)
