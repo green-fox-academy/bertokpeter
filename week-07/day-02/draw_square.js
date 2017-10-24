515363a1ec3d876e1e7b27dc66cdb3be5e7702e5
bertokpeter <bertokpeter92@gmail.com>
2017-10-24 16:46:51 +0200

    feat(square): prints square

---
+++ week-07/day-02/draw_square.js
@@ -0,0 +1,23 @@
+'use strict';
+
+let lineCount = 9;
+
+// Write a program that draws a
+// square like this:
+//
+//
+// %%%%%
+// %   %
+// %   %
+// %   %
+// %   %
+// %%%%%
+//
+// The square should have as many lines as lineCount is
+for (let i = 1; i < lineCount+1; ++i) {
+    if (i === 1 || i === lineCount) {
+        console.log("%".repeat(lineCount-1))
+    } else {
+        console.log("%" + " ".repeat(lineCount-3) + "%")
+    }
+}
