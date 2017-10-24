befaa6ca390cd6ed9e532be19afa9ffa3ccec395
bertokpeter <bertokpeter92@gmail.com>
2017-10-24 16:51:27 +0200

    fix(square): semicolons

--- week-07/day-02/draw_square.js
+++ week-07/day-02/draw_square.js
@@ -16,8 +16,8 @@ let lineCount = 9;
 // The square should have as many lines as lineCount is
 for (let i = 1; i < lineCount+1; ++i) {
     if (i === 1 || i === lineCount) {
-        console.log("%".repeat(lineCount-1))
+        console.log("%".repeat(lineCount-1));
     } else {
-        console.log("%" + " ".repeat(lineCount-3) + "%")
+        console.log("%" + " ".repeat(lineCount-3) + "%");
     }
 }
