66fd2be25949cb0fe248ae8d3b6ed5739515aea5
bertokpeter <bertokpeter92@gmail.com>
2017-10-24 16:57:53 +0200

    feat(diagonal): prints diagonal

---
+++ week-07/day-02/draw_diagonal.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let lineCount = 11;
+
+
+// Write a program that draws a
+// square like this:
+//
+//
+// %%%%%
+// %%  %
+// % % %
+// %  %%
+// %   %
+// %%%%%
+//
+// The square should have as many lines as lineCount is
+
+for (let i = 1; i < lineCount+1; ++i) {
+    if (i === 1 || i === lineCount) {
+        console.log("%".repeat(lineCount-1));
+    } else if (i < lineCount-1) {
+        console.log("%" + " ".repeat(i-2) + "%" + " ".repeat(lineCount-2-i) + "%");
+    } else {
+        console.log("%" + " ".repeat(lineCount-3) + "%")
+    }
+}
