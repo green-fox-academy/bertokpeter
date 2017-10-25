72d8f2defdc19706b790758d34023f8fba4d5f6d
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 09:36:22 +0200

    feat(doubling):doublinf function

---
+++ week-07/day-03/doubling.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a variable named `ak` and assign the value `123` to it
+// - Create a function called `doubling` that doubles it's input parameter
+// - Log the result of `doubling(ak)` to the console
+let ak = 123;
+
+function doubling(number) {
+    let doubled = number*2
+    return doubled
+}
+
+console.log(doubling(ak))
