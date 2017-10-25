4d5214f4a0f266add8296e871481eab2f113dbce
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 16:39:35 +0200

    feat(reverse_string): reverse string

---
+++ week-07/day-03/reverse_string.js
@@ -0,0 +1,9 @@
+'use strict';
+// Reverse the string!
+
+let reversed = ".eslaf eb t'ndluow ecnetnes siht ,dehctiws erew eslaf dna eurt fo sgninaem eht fI";
+
+let rev_list = reversed.split('');
+rev_list.reverse();
+let text = rev_list.join('');
+console.log(text);
