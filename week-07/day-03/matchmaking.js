52d78530bbd5ae4577c0fd9dfc51f1b129a81045
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 16:58:57 +0200

    feat(matchmaking): matchmaking list

---
+++ week-07/day-03/matchmaking.js
@@ -0,0 +1,18 @@
+'use strict';
+// Join the two array by matching one girl with one boy in the order array
+// Exepected output: ["Eve", "Joe", "Ashley", "Fred"...]
+
+let girls = ["Eve","Ashley","Bözsi","Kat","Jane"];
+let boys = ["Joe","Fred","Béla","Todd","Neef","Jeff"];
+let order = [];
+
+for (let i = 0; i < Math.max(girls.length, boys.length); i++) {
+    if (girls.length > i) {
+        order.push(girls[i]);
+    }
+    if (boys.length > 0) {
+        order.push(boys[i]);
+    }
+}
+
+console.log(order);
