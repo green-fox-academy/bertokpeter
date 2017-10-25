05c45e0608078c4a8bbc1872dfa29708e2ae93fb
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 11:15:23 +0200

    feat(factorio): factorial func

---
+++ week-07/day-03/factorio.js
@@ -0,0 +1,12 @@
+'use strict';
+// - Create a function called `factorio`
+//   that returns it's input's factorial
+function factorio(number) {
+    if (number <= 1) {
+        return 1
+    } else {
+        return number * factorio(number-1)
+    }
+}
+
+console.log(factorio(3))
