7cc385c944d1e1a58fee7f7b01d03288971c8a91
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 14:56:22 +0200

    feat(sumall): sums all the elements

---
+++ week-07/day-03/suma_all.js
@@ -0,0 +1,10 @@
+'use strict';
+// - Create a variable named `ai` with the following content: `[3, 4, 5, 6, 7]`
+// - Log the sum of the elements in `ai` to the console
+let ai = [3, 4, 5, 6, 7];
+
+let sum = ai.reduce(function(prev,next) {
+    prev = prev + next;
+    return prev;
+});
+console.log(sum);
