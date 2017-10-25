a0a19105d09e5d977206655a04678393a0b4681b
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 16:32:51 +0200

    feat(studentcounter): print number of student candies, sums age

---
+++ week-07/day-03/student_counter.js
@@ -0,0 +1,36 @@
+'use strict';
+
+let students = [
+        {'name': 'Teodor', 'age': 3, 'candies': 2},
+        {'name': 'Rezso', 'age': 9.5, 'candies': 2},
+        {'name': 'Zsombor', 'age': 12, 'candies': 5},
+        {'name': 'Aurel', 'age': 7, 'candies': 3},
+        {'name': 'Olaf', 'age': 12, 'candies': 7},
+        {'name': 'Gerzson', 'age': 10, 'candies': 1},
+]
+
+// create a function that takes a list of students and logs: 
+// - how many candies are owned by students
+
+// create a function that takes a list of students and logs:
+// - Sum of the age of people who have lass than 5 candies
+
+function candy_counter(student_log) {
+    student_log.forEach(function(e) {
+        console.log(e["name"], "has", e["candies"], "candies");
+    });
+}
+
+candy_counter(students);
+
+function sum_candies(student_log) {
+    let sum = 0;
+    student_log.forEach(function(e) {
+        if (e["candies"] < 5) {
+            sum += e["age"]
+        }
+    });
+    return sum
+}
+
+console.log(sum_candies(students))
