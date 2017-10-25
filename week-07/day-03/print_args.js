e078120212a080f0e8184d92bb2a8572de5959c1
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 12:03:14 +0200

    feat(print_args): func print all arguments

---
+++ week-07/day-03/print_args.js
@@ -0,0 +1,9 @@
+'use strict';
+// - Create a function called `printer`
+//   which logs to the console the input parameters
+//   (can have multiple number of arguments)
+function printer(...string) {
+    console.log(string.join(' '));
+}
+
+printer('hey', 'Peti', 'hey', 'Ati');
