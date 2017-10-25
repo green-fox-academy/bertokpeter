6fe6e0e2e57817515d3aa5f822a30d951c57175a
bertokpeter <bertokpeter92@gmail.com>
2017-10-25 13:47:58 +0200

    feat(swap_elems): swap 2nd a 3rd element

---
+++ week-07/day-03/swap_elements.js
@@ -0,0 +1,6 @@
+'use strict';
+// - Create a variable named `abc` with the following content: `["Arthur", "Boe", "Chloe"]`
+// - Swap the first and the third element of `abc`
+let abc = ["Arthur", "Boe", "Chloe"];
+abc [1] = [abc[2], abc[2]=abc[1]][0]
+console.log(abc)
