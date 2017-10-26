44ff5fe86ee099cd7ae9808e8b3fdae8e0e49cf6
bertokpeter <bertokpeter92@gmail.com>
2017-10-26 20:04:40 +0200

    refact(events1js): toggle method

--- week-07/day-04/events-1.js
+++ week-07/day-04/events-1.js
@@ -1,13 +1,17 @@
 'use strict';
 
 let button = document.querySelector('button');
-let bod = document.querySelector('body');
-function changebg() {
-    if (bod.classList.contains('party')){
-        bod.classList.remove('party')
-    } else {
-        bod.classList.add('party');
-    }
+let bod = document.querySelector('div');
+
+function changeBg() {
+    bod.classList.toggle('party')
 }
 
-button.addEventListener('click', changebg);
+button.addEventListener('click', changeBg);
+// function changebg() {
+//     if (bod.classList.contains('party')){
+//         bod.classList.remove('party')
+//     } else {
+//         bod.classList.add('party');
+//     }
+// }
