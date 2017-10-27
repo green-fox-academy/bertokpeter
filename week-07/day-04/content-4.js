9b08ea788d2da8db231f688038400ece16e1a674
bertokpeter <bertokpeter92@gmail.com>
2017-10-27 09:56:46 +0200

    refact(content-4js): some refacts

--- week-07/day-04/content-4.js
+++ week-07/day-04/content-4.js
@@ -1,11 +1,15 @@
 'use strict';
 
 let things = ['apple', 'banana', 'cat', 'dog'];
-let lis = document.querySelectorAll('li');
+let items = document.getElementsByTagName('li');
 
-lis.forEach(function(e,i) {
-    e.textContent = things[i];
-});
+for (let i = 0; i < items.length; i++) {
+    items[i].textContent = things[i];
+}
 
-let ul = document.querySelector('ul');
-ul.style.backgroundColor = 'limegreen';
+// items.forEach(function(e,i) {
+//     e.textContent = things[i];
+// });
+
+// let ul = document.querySelector('ul');
+// ul.style.backgroundColor = 'limegreen';
