5eac919d7d0a07209086e45f389226d60fc5faab
bertokpeter <bertokpeter92@gmail.com>
2017-10-30 10:19:34 +0100

    feat(money): shuffle money among panama and cyprus

---
+++ week-08/day-01/money_shuffle.js
@@ -0,0 +1,34 @@
+const Panama = {
+    cash: 0,
+    name: 'Panama',
+    tax: '1%',
+    deposit: function(amt) {
+        this.cash += amt;
+    }
+}
+
+const Cyprus = {
+    cash: 0,
+    name: 'Cyprus',
+    tax: '5%',
+    deposit: function(amt) {
+        this.cash += amt;
+    }
+}
+
+const Shuffler = {
+    cash: 10000,
+    pick: function(name) {
+        this.cash -= 1000;
+        name.deposit(1000);
+        console.log(name.name + ' got 1000' );
+    }
+}
+
+Shuffler.pick(Panama) // prints Panama got 1000
+Shuffler.pick(Cyprus) // prints Cyprus got 1000
+Shuffler.pick(Panama) // prints Panama got 1000
+Shuffler.pick(Cyprus) // prints Cyprus got 1000
+
+console.log( Panama.cash ) // 2000 
+console.log( Cyprus.cash ) // 2000 
