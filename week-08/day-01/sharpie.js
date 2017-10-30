8f7d3c484548b55c396e02e1cef3fe04c198167e
bertokpeter <bertokpeter92@gmail.com>
2017-10-30 12:35:51 +0100

    refact(sharpie): sharpie can deplete fully

--- week-08/day-01/sharpie.js
+++ week-08/day-01/sharpie.js
@@ -4,11 +4,14 @@ function Sharpie(color, width) {
     this.inkAmount = 100;
     this.use = function(){
         this.inkAmount -= this.width;
+        if (this.inkAmount < 0) {
+            this.inkAmount = 0;
+        }
     };
 }
 
-let sharp = new Sharpie('red', 3);
-while (sharp.inkAmount >= sharp.width) {
+let sharp = new Sharpie('red', 55);
+while (sharp.inkAmount > 0) {
     sharp.use();
     console.log(sharp.inkAmount);
 }
