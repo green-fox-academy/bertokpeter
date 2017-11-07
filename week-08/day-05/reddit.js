63a2b1f4947ed17c3413d93b60fa6a26d96ee08b
bertokpeter <bertokpeter92@gmail.com>
2017-11-07 20:29:34 +0100

    refact(redditjs): innehtml at creathml modified

--- week-08/day-05/reddit.js
+++ week-08/day-05/reddit.js
@@ -1,7 +1,6 @@
 'use strict';
 let redditRequest = new XMLHttpRequest();
 let mainSection = document.querySelector('section.posts');
-// redditRequest.open()
 
 function talkToAPI (method, callback) {
     let url = 'http://secure-reddit.herokuapp.com/simple/posts'
@@ -17,23 +16,39 @@ function talkToAPI (method, callback) {
 
 function listPosts(json){
     json.posts.forEach(function(element) {
-        createHTML(element.url, element.title, calculateTime(element.timestamp));
+        createHTML(element.url, element.title, calculateTime(element.timestamp), element.user);
     });
 }
 
-function createHTML (url, title, timestamp) {
+function createHTML (url, title, timestamp, user) {
     let post = document.createElement('div');
     post.classList.add('post');
-    post.innerHTML = `<div class="voter"><img src="assets/upvote.png"/><span>0</span><img src="assets/downvote.png"/>
-                     </div><div class="post-content"><a href="` + url + `">` + title +`</a><p>submitted ` + timestamp + ` mins ago by
-                     anonymus</p><a class="options" href="">Modify</a><a class="options" href="">Remove</a></div>`;
+    let username = user;
+    if (username === null) {
+        username = 'anonymus'
+    }
+    post.innerHTML = `<div class="voter">
+                        <img src="assets/upvote.png"/>
+                        <span>0</span>
+                        <img src="assets/downvote.png"/>
+                     </div>
+                     <div class="post-content">
+                       <a href="` + url + `">` + title +`</a>
+                       <p>submitted ` + timestamp + ` mins ago by ` + username + ` </p>
+                       <a class="options" href="">Modify</a>
+                       <a class="options" href="">Remove</a></div>`;
     mainSection.appendChild(post);
 }
 
 function calculateTime (timestamp) {
-    let elapsedTime = Math.floor(timestamp/60);
+    let elapsedTime = Math.floor(timestamp/1000/60);
     return elapsedTime;
 }
 
 talkToAPI('GET', listPosts);
 
+let postTitle = document.getElementById('title');
+let postUrl = document.getElementById('url');
+let submitButton = document.querySelector('button.submit');
+
+submitButton.addEventListener('click',)
