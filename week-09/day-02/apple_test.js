4f00364ac57531c113983da5d47dc60f0e638ae7
bertokpeter <bertokpeter92@gmail.com>
2017-11-07 20:32:08 +0100

    refact(appletest): tests for sum func

--- week-09/day-02/apple_test.js
+++ week-09/day-02/apple_test.js
@@ -1,9 +1,40 @@
 'use strict';
 
 let test = require('tape');
-let apple = require('./apple.js');
+let appleClass = require('./apple.js');
 
 test('concat strings', function (t) {  
+    let apple = new appleClass();
     t.equal(apple.getApple(), 'appl');
+    t.end();
+});
+
+test('summ array', function(t) {
+    let apple = new appleClass();
+    t.equal(apple.sum([1,2,3,4,5]), 15);
+    t.end();
+});
+
+test('summ empty', function(t) {
+    let apple = new appleClass();
+    t.equal(apple.sum([]), 0);
+    t.end();
+});
+
+test('summ one', function(t) {
+    let apple = new appleClass();
+    t.equal(apple.sum([5]), 5);
+    t.end();
+});
+
+test('summ null', function(t) {
+    let apple = new appleClass();
+    t.equal(apple.sum(null), 0);
+    t.end();
+});
+
+test('summ string', function(t) {
+    let apple = new appleClass();
+    t.error(apple.sum(null), 0);
     t.end();
 });
