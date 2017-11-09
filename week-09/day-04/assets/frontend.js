7e5a683a685cd5f3fc331762c298ae140c415ca5
bertokpeter <bertokpeter92@gmail.com>
2017-11-09 17:47:08 +0100

    refact(frontend): render html

--- week-09/day-04/assets/frontend.js
+++ week-09/day-04/assets/frontend.js
@@ -4,17 +4,23 @@ const xml = new XMLHttpRequest();
 const body = document.querySelector('body');
 let url = "http://localhost:3000";
 
-function talkToAPI(method, resource){
+function talkToAPI(method, resource, callback){
     xml.open(method, url + resource, true);
     xml.onload = function(){
-        body.innerHTML = xml.response;
+        callback(xml.response);
     }
     xml.send();
 }
 
+talkToAPI('GET', '/list', createList);
 
-function getList(){
-    talkToAPI('GET', '/list')
+function createList(response){
+    const list = document.querySelector('div.list');
+    list.innerHTML = response;
+    talkToAPI('GET', '/all?plt=340', createTable);
 }
 
-getList();
+function createTable(response){
+    const table = document.querySelector('div.table');
+    table.innerHTML = response;
+}
