b407a16333568ce0a4cc2e38985fbc388808224d
bertokpeter <bertokpeter92@gmail.com>
2017-11-09 23:18:49 +0100

    feat(redditjs):psot mehtod

--- week-08/day-05/reddit.js
+++ week-08/day-05/reddit.js
@@ -1,26 +1,35 @@
 'use strict';
 let redditRequest = new XMLHttpRequest();
 let mainSection = document.querySelector('section.posts');
+let url = 'http://secure-reddit.herokuapp.com/simple/posts'
 
-function talkToAPI (method, callback) {
-    let url = 'http://secure-reddit.herokuapp.com/simple/posts'
+function talkToAPI (method, url, reqBody, callback) {
     redditRequest.open(method, url, true);
     redditRequest.setRequestHeader('accept', 'application/json');
+    if (method === 'POST') {
+        redditRequest.setRequestHeader('content-type', 'application/json');
+    }
     redditRequest.onload = function(){
         let posts = JSON.parse(redditRequest.response);
-        console.log(posts)
         callback(posts);
     };
-    redditRequest.send();
+    redditRequest.send(JSON.stringify(reqBody));
 }
 
 function listPosts(json){
+    console.log(json);
     json.posts.forEach(function(element) {
-        createHTML(element.url, element.title, calculateTime(element.timestamp), element.user);
+        createHTML(element.score, element.url, element.title, calculateTime(element.timestamp), element.user);
+    });
+    let removers = document.querySelectorAll('a.remove');
+    json.posts.forEach(function(element,i){
+        removers[i].addEventListener('click', function(){
+            talkToAPI('DELETE', url, null, del)
+        });
     });
 }
 
-function createHTML (url, title, timestamp, user) {
+function createHTML (score, url, title, timestamp, user) {
     let post = document.createElement('div');
     post.classList.add('post');
     let username = user;
@@ -28,15 +37,15 @@ function createHTML (url, title, timestamp, user) {
         username = 'anonymus'
     }
     post.innerHTML = `<div class="voter">
-                        <img src="assets/upvote.png"/>
-                        <span>0</span>
-                        <img src="assets/downvote.png"/>
+                        <img class="upvote" src="assets/upvote.png"/>
+                        <span>` + score + `</span>
+                        <img class="downvote" src="assets/downvote.png"/>
                      </div>
                      <div class="post-content">
                        <a href="` + url + `">` + title +`</a>
                        <p>submitted ` + timestamp + ` mins ago by ` + username + ` </p>
                        <a class="options" href="">Modify</a>
-                       <a class="options" href="">Remove</a></div>`;
+                       <a class="options remove">Remove</a></div>`;
     mainSection.appendChild(post);
 }
 
@@ -45,10 +54,24 @@ function calculateTime (timestamp) {
     return elapsedTime;
 }
 
-talkToAPI('GET', listPosts);
+talkToAPI('GET', url, null, listPosts);
 
 let postTitle = document.getElementById('title');
 let postUrl = document.getElementById('url');
 let submitButton = document.querySelector('button.submit');
 
-submitButton.addEventListener('click',)
+submitButton.addEventListener('click', function(){
+    let reqBody = {
+        "title": postTitle.value,
+        "url": postUrl.value
+    }
+    talkToAPI('POST', url, reqBody, postPost);
+});
+
+function postPost(json){
+    window.location.href = 'reddit.html';
+}
+
+function del(json){
+    console.log(json);
+}
