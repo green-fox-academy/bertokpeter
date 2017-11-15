ccd1464946fa04930b1b16cef5b319dbe7a93aeb
bertokpeter <bertokpeter92@gmail.com>
2017-11-15 14:19:39 +0100

    feat(foxserver):mocked backend

--- week-10/foxplayer/foxserver.js
+++ week-10/foxplayer/foxserver.js
@@ -10,4 +10,18 @@ app.get('/', function (req, res){
     res.sendFile(__dirname + '/foxplayer.html')
 });
 
+let playlists = {
+    playlists:[
+        { "id": 1, "title": "All tracks", "system": 1},
+        { "id": 2, "title": "Favorites", "system": 1},
+        { "id": 3, "title": "Music for programming", "system": 0},
+        { "id": 4, "title": "Driving", "system": 0},
+        { "id": 5, "title": "Fox house", "system": 0},
+    ]
+};
+
+app.get('/playlists', function(req, res) {
+    res.json(playlists);
+});
+
 app.listen(5000);
