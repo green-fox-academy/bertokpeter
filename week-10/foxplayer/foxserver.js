8fdb9d2a2c974183dc049b7a99e6b1243545458b
bertokpeter <bertokpeter92@gmail.com>
2017-11-15 17:57:09 +0100

    feat(foxserver): tracklist endpoints

--- week-10/foxplayer/foxserver.js
+++ week-10/foxplayer/foxserver.js
@@ -4,6 +4,7 @@ const express = require('express');
 const app = express();
 
 app.use('/assets', express.static('./assets'));
+app.use('/tracks', express.static('./tracks'));
 app.use(express.json());
 
 app.get('/', function (req, res){
@@ -20,8 +21,29 @@ let playlists = {
     ]
 };
 
+let tracklist = {
+    tracklist:[
+        { "id": 1, "title": "Warped", "artist": "Mutato Muzika", "duration": 80, "path": "tracks/01_-_Crash_Bandicoot_3_Warped.mp3", "playlists": []},
+        { "id": 2, "title": "Toad Village", "artist": "Mutato Muzika", "duration": 108, "path": "tracks/02_-_Toad_Village.mp3", "playlists": ["2"]}
+    ]
+}
+
 app.get('/playlists', function(req, res) {
     res.json(playlists);
+});
+
+app.get('/playlist-tracks', function(req, res) {
+    res.json(tracklist);
+});
+
+app.get('/playlist-tracks/:playlist_id', function(req, res) {
+    let responseTrackList = tracklist.tracklist.filter(function(element){
+        return element.playlists.includes(req.params.playlist_id);
+    });
+    let responseJSON = {
+        tracklist: responseTrackList
+    }
+    res.json(responseJSON);
 });
 
 app.listen(5000);
