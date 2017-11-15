9c95228f85b50ee3c5b50a08c4da05ecc9eb334c
bertokpeter <bertokpeter92@gmail.com>
2017-11-15 18:02:08 +0100

    refact(ajax): new parameter

--- week-10/foxplayer/assets/ajax.js
+++ week-10/foxplayer/assets/ajax.js
@@ -2,11 +2,11 @@
 const ajax = function (){
     const playerRequest = new XMLHttpRequest();
 
-    function talkToAPI (method, url, content, callback) {
+    function talkToAPI (method, url, content, callback, setvariable) {
         playerRequest.open(method, url, true);
         playerRequest.onload = function(){
             let list = JSON.parse(playerRequest.response);
-            callback(list);
+            callback(list, setvariable);
         };
         playerRequest.send(JSON.stringify(content));
     }
