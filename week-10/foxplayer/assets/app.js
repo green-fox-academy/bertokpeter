0124c3969c3fb62d99c1762e421ed0c7710426f9
bertokpeter <bertokpeter92@gmail.com>
2017-11-15 14:22:37 +0100

    feat(app):instantiate ajax and playlists

--- week-10/foxplayer/assets/app.js
+++ week-10/foxplayer/assets/app.js
@@ -1,1 +1,11 @@
 'use strcit';
+
+class App {
+    constructor(){
+        const myAjax = ajax();
+        const myPlaylists = playlists();
+        myAjax.talkToAPI('GET', "http://localhost:5000/playlists", null, myPlaylists.load);
+    }
+}
+
+const application = new App();
