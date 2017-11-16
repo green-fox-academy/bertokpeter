464fced2ad85dc1a4f85a93bd06a34fd33a7bd56
bertokpeter <bertokpeter92@gmail.com>
2017-11-16 16:09:23 +0100

    feat(control panel): btnpress methods, addevents

--- week-10/foxplayer/assets/control_panel.js
+++ week-10/foxplayer/assets/control_panel.js
@@ -1,12 +1,44 @@
 'use strict';
 const controlPanel = function(){
     const audio = document.querySelector('audio');
+    // let onEndAction;
+    const rewind = document.querySelector('.rewind');
+    const forward = document.querySelector('.forward');
+    let forwardClickAction;
+    let prevClickAction;
 
-    function playTrack(src){
-        audio.setAttribute('src', src);
+    function playTrack(track){
+        audio.setAttribute('src', track.path);
     }
 
+    function addEvents(){
+        // audio.addEventListener('ended', function(){
+        //     onEndAction()
+        // });
+        forward.addEventListener('click',function(){
+            forwardClickAction();
+        });
+        rewind.addEventListener('click', function(){
+            prevClickAction();
+        });
+    }
+
+    // function endEvent(callback){
+    //     onEndAction = callback;
+    // }
+
+    function nextBtnPressed(callback){
+        forwardClickAction = callback;
+    }
+
+    function prevBtnPressed(callback){
+        prevClickAction = callback;
+    }
+
+    addEvents();
     return {
-        playTrack
+        playTrack,
+        nextBtnPressed,
+        prevBtnPressed
     }
 };
