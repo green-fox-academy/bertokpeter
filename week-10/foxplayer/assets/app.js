6300e87e59acef174af5ab0779ce4a4a9c14a3e5
bertokpeter <bertokpeter92@gmail.com>
2017-11-16 12:30:40 +0100

    refact(app): deleted intervals, shortened

--- week-10/foxplayer/assets/app.js
+++ week-10/foxplayer/assets/app.js
@@ -2,31 +2,14 @@
 
 class App {
     constructor(){
-        const myAjax = ajax();
         const myPanel = controlPanel();
         const myPlayLists = playLists();
         const myTrackList = trackList();
-        myAjax.talkToAPI('GET', "http://localhost:5000/playlists", null, myPlayLists.load, setPlaylists);
-        let loadTracks = setInterval(function(){
-            if(document.querySelectorAll('div.playlist').length > 0){
-                myAjax.talkToAPI('GET', "http://localhost:5000/playlist-tracks", null, myTrackList.load, setTracks);
-                clearInterval(loadTracks);
-            } 
-        },0);
-        let loadPlay = setInterval(function(){
-            if(document.querySelectorAll('div.track').length > 0){
-                myPanel.playTrack(myTrackList.tracks.tracklist[0].path);
-                clearInterval(loadPlay);
-            }
-        },0);
 
-        function setTracks(list){
-            myTrackList.tracks = list;
-        }
+        myTrackList.firstHandler(myPanel.playTrack);
+        myTrackList.load();
 
-        function setPlaylists(list){
-            myPlayLists.playlists = list;
-        }
+        myTrackList.clickHandler(myPanel.playTrack);
     }
 }
 
