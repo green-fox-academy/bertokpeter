a4d0712af41bd0348c3ea41edb5ab7061e8ae302
bertokpeter <bertokpeter92@gmail.com>
2017-11-16 22:19:10 +0100

    feat(ajax): set headers

--- week-10/foxplayer/assets/ajax.js
+++ week-10/foxplayer/assets/ajax.js
@@ -4,8 +4,10 @@ const ajax = function (){
 
     function xml(method, url, callback, content=null) {
         playerRequest.open(method, url, true);
+        playerRequest.setRequestHeader('accept', 'application/json');
+        playerRequest.setRequestHeader('content-type', 'application/json');
         if (method !== 'GET'){
-            content = JSON.stringify(content)
+            content = JSON.stringify(content);
         }
         playerRequest.onload = function(){
             let list = JSON.parse(playerRequest.response);
